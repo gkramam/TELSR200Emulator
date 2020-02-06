82dc8e059e43bb33f2165cf06d264e8c86838ad1
Ramam <gkramam@gmail.com>
2020-02-06 11:24:17 -0500

    CheckSum

---
+++ TELSR200Emulator/CheckSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TELSR200Emulator
+{
+    public static class CheckSum
+    {
+        public static string Compute(string unicodeMsg)
+        {
+            try
+            {
+                //unicodeMsg = ",1,INIT,1,1,G,";
+                byte[] asciiBytes = ASCIIEncoding.ASCII.GetBytes(unicodeMsg);
+                //var hexString = BitConverter.ToString(asciiBytes);
+                int count = 0;
+                for (int i = 0; i < asciiBytes.Length; i++)
+                {
+                    count += asciiBytes[i];
+                }
+
+                var hexsumBytes = BitConverter.GetBytes(count);
+                var hexsumString = BitConverter.ToString(hexsumBytes, 0, 1);
+                return hexsumString;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                throw;
+            }
+        }
+    }
+}
